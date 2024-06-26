d000045162f41e9d8af8eb0519fa95e4de588a7f
Agustin Moran <agustin.moranr@gmail.com>
2024-06-26 13:53:27 -0600

    clase 2: añade suporte para compilar módulos con babel en los test de jest

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,9 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
+const isTestEnv = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', {modules: false}],
+    ['@babel/preset-env', {modules: isTestEnv ? 'commonjs' : false}],
     '@babel/preset-react',
     [
       '@emotion/babel-preset-css-prop',
